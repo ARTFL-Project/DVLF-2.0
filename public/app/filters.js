9e158f210c21128c35bd223e3c4abf6a9db5be17
Clovis <clovisgladstone@gmail.com>
2016-08-17 11:26:33 -0500

    order examples by vote on the client: keep ordering of examples of score=0

--- public/app/filters.js
+++ public/app/filters.js
@@ -5,7 +5,8 @@
         .module("DVLF")
         .filter("isEmpty", isEmpty)
         .filter("unsafe", unSafe)
-        .filter('encodeURIComponent', encode);
+        .filter('encodeURIComponent', encode)
+        .filter("sortByVote", sortByVote);
 
 
     function isEmpty() {
@@ -24,5 +25,31 @@
 
     function encode($window) {
         return $window.encodeURIComponent;
+    }
+
+    function sortByVote() {
+        return function(examples) {
+            var orderedExamples = [];
+            var negativeExamples = [];
+            var otherExamples = [];
+            for (var i=0; i < examples.length; i+=1) {
+                if (examples[i].score > 0) {
+                    orderedExamples.push(examples[i]);
+                } else if (examples[i].score < 0){
+                    negativeExamples.push(examples[i])
+                } else {
+                    otherExamples.push(examples[i]);
+                }
+            }
+            orderedExamples.sort(function(a, b) {
+                return b.score - a.score;
+            });
+            negativeExamples.sort(function(a, b) {
+                return b.score - a.score;
+            });
+
+            var allExamples = orderedExamples.concat(otherExamples, negativeExamples);
+            return allExamples;
+        }
     }
 })();
